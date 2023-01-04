f0c1fa4d6ac22e1767548a37b184aea653c7eaa4
Tonny051111 <70208128+Tonny051111@users.noreply.github.com>
2023-01-04 21:14:58 +0800

    Update 电脑配置查询.py

--- 电脑配置查询.py
+++ 电脑配置查询.py
@@ -1,7 +1,6 @@
 # 获取计算机信息
 import wmi
 
-
 def System_spec():
     Pc = wmi.WMI()
     os_info = Pc.Win32_OperatingSystem()[0]
@@ -10,12 +9,10 @@ def System_spec():
     os_name = os_info.Name.encode('utf-8').split(b'|')[0]
     ram = float(os_info.TotalVisibleMemorySize) / 1048576
 
+    print("\n计算机信息：")
     print(f'操作系统: {os_name}')
     print(f'CPU: {processor.Name}')
     print(f'内存: {ram} GB')
     print(f'显卡: {Gpu.Name}')
-
-    print("\n计算机信息如上 ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑")
-
 
 System_spec()
